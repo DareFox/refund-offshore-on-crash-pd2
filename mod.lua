9afa906e4290595c4806e6ed59bc86349f59f9bf
DareFox <easymalink@gmail.com>
2023-08-09 13:48:53 +0300

    mod.lua: Put functions to Util and RefundMod class

--- mod.lua
+++ mod.lua
@@ -1,33 +1,9 @@
-local RefundSavePath = ModPath .. "crash_save_cashback.json"
-local CrashlogPath = Application:nice_path(os.getenv("LOCALAPPDATA") .. '/PAYDAY 2/', true) .. 'crashlog.txt'
+------------
+--- UTIL ---
+------------
+local Util = {}
 
-local function Log(...)
-    log("[REFUND_CRASH]", ...)
-end
-
-local function LoadSave()
-    local file = io.open(RefundSavePath, "r")
-    local save = nil
-    if file then
-        Log("Can read save, decoding it")
-        save = json.decode(file:read("*all"))
-        file:close()
-    end
-
-    -- if file is failed to open or file is empty
-    if save == nil then
-        Log("Save is empty or I can't open it")
-        save = {
-            PreviousCrashHash = '',
-            IsJobActive = false,
-            OffshoreMoneySpend = nil,
-        }
-    end
-
-    return save
-end
-
-local function CreateObserverTable(table, callback)
+function Util.CreateObserverTable(table, callback)
     local observer = {}
     local mt = {
         __index = table,
@@ -40,88 +16,127 @@ local function CreateObserverTable(table, callback)
     return observer
 end
 
-local function SaveJson(table)
-    local Save = io.open(RefundSavePath, "w+")
+
+function Util.Log(...)
+    local args = { ... }
+    local first = table.remove(args, 1)
+    log("[REFUND CRASH]: " .. tostring(first), unpack(args))
+end
+
+function Util.addOffshore(money)
+    Util.Log("Current offshore: " .. tostring(managers.money:offshore()))
+    managers.money:add_to_offshore(RefundMod.Status.OffshoreMoneySpend)
+    Util.Log("Returned: " .. RefundMod.Status.OffshoreMoneySpend .. "$ offshore money from crash")
+    Util.Log("After offshore" .. tostring(managers.money:offshore()))
+end
+
+Util.Log("Hello everynya!")
+----------------
+-- REFUND MOD --
+----------------
+_G.RefundMod = _G.RefundMod or {}
+RefundMod.ModPath = ModPath -- cache it
+RefundMod.SavePath = ModPath .. "crash_save_cashback.json"
+RefundMod.CrashlogPath = Application:nice_path(os.getenv("LOCALAPPDATA") .. '/PAYDAY 2/', true) .. 'crashlog.txt'
+
+
+function RefundMod:Save()
+    local Save = io.open(self.SavePath, "w+")
     if Save then
-        Save:write(json.encode(table))
+        Save:write(json.encode(self.Status))
         Save:close()
     end
 end
 
-local RefundStatus = CreateObserverTable(LoadSave(), function(table, ...)
-    Log("Refund status changed, saving file")
-    SaveJson(table)
-end)
+function RefundMod:Load()
+    local file = io.open(self.SavePath, "r")
+    local status = nil
+    if file then
+        Util.Log("Can read save, decoding it")
+        status = json.decode(file:read("*all"))
+        file:close()
+    end
 
-local function getHashOfCrash()
-    if io.file_is_readable(CrashlogPath) then
-        return file.FileHash(CrashlogPath)
+    if status == nil then
+        Util.Log("Save is empty or I can't open it")
+        status = {
+            PreviousCrashHash = '',
+            IsJobActive = false,
+            OffshoreMoneySpend = nil,
+        }
+    end
+
+    return Util.CreateObserverTable(status, function ()
+        self:Save()
+    end)
+end
+    
+function RefundMod:getHashOfCrash()
+    if io.file_is_readable(self.CrashlogPath) then
+        return file.FileHash(self.CrashlogPath)
     else
         return nil
     end
 end
 
-local function onWillfulContractTermination()
-    Log("Player has terminated contract willfully. Reset cashbacks status")
-    RefundStatus.OffshoreMoneySpend = nil
-    RefundStatus.IsJobActive = false
+function RefundMod:onWillfulContractTermination()
+    Util.Log("Player has terminated contract willfully. Reset cashbacks status")
+    self.Status.OffshoreMoneySpend = nil
+    self.Status.IsJobActive = false
 end
 
-local function addOffshore(money)
-    Log("Current offshore: " .. tostring(managers.money:offshore()))
-    managers.money:add_to_offshore(RefundStatus.OffshoreMoneySpend)
-    Log("Returned: " .. RefundStatus.OffshoreMoneySpend .. "$ offshore money from crash")
-    Log("After offshore" .. tostring(managers.money:offshore()))
-end
+RefundMod.Status = RefundMod:Load()
 
--- HOOKS
+-------------
+--- HOOKS ---
+-------------
 Hooks:PostHook(MoneyManager, "on_buy_premium_contract", "REFUND_CRASH_ON_CONTRACT_BUY",
     function(self, job_id, difficulty_id)
         local offshoreSpend = self:get_cost_of_premium_contract(job_id, difficulty_id)
-        RefundStatus.OffshoreMoneySpend = offshoreSpend
-        RefundStatus.IsJobActive = true
-        Log(tostring(offshoreSpend) .. "$ offshore was spend on contract " .. job_id)
+        RefundMod.Status.OffshoreMoneySpend = offshoreSpend
+        RefundMod.Status.IsJobActive = true
+        Util.Log(tostring(offshoreSpend) .. "$ offshore was spend on contract " .. job_id)
     end
 )
 
 Hooks:PreHook(MenuCallbackHandler, "load_start_menu_lobby", "REFUND_CRASH_ABORT", function()
-    onWillfulContractTermination()
+    RefundMod:onWillfulContractTermination()
 end)
 
 Hooks:PreHook(MenuCallbackHandler, "_dialog_end_game_yes", "REFUND_CRASH_END_GAME_YES", function()
-    onWillfulContractTermination()
+    RefundMod:onWillfulContractTermination()
 end)
 
 Hooks:PreHook(MenuManager, "on_leave_lobby", "REFUND_CRASH_ON_LEAVE_LOBBY", function()
-    onWillfulContractTermination()
+    RefundMod:onWillfulContractTermination()
 end)
 
 Hooks:PostHook(MoneyManager, "load", "REFUND_CRASH_SETUP", function()    
     --- START UP 
-    local currentHash = getHashOfCrash()
-    Log("hash: " .. currentHash)
+    local currentHash = RefundMod:getHashOfCrash()
+    Util.Log("hash: " .. currentHash)
 
     if currentHash == nil then
-        Log("Can't get hash of crash. Crash file doesn't exists or isn't readable")
+        Util.Log("Can't get hash of crash. Crash file doesn't exists or isn't readable")
         return
     end
 
-    if currentHash == RefundStatus.PreviousCrashHash then
-        Log("Identical hash, no new crash")
+    if currentHash == RefundMod.Status.PreviousCrashHash then
+        Util.Log("Identical hash, no new crash")
         return
     end
 
 
-    if not (RefundStatus.IsJobActive or false) then 
-        Log("Job wasnt active at crash")
+    if not (RefundMod.Status.IsJobActive or false) then 
+        Util.Log("Job wasnt active at crash")
         return
     end
 
-    if not (RefundStatus.OffshoreMoneySpend or false) then 
-        Log("Spent offshore is empty")
+    if not (RefundMod.Status.OffshoreMoneySpend or false) then 
+        Util.Log("Spent offshore is empty")
         return
     end
 
-    addOffshore(RefundStatus.OffshoreMoneySpend)
-    RefundStatus.PreviousCrashHash = currentHash
+    Util.addOffshore(RefundMod.Status.OffshoreMoneySpend)
+    RefundMod.Status.PreviousCrashHash = currentHash
 end)
