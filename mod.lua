962173bb8c9af98189aa7b47e2ac81e3a59972b0
DareFox <easymalink@gmail.com>
2023-08-09 19:44:15 +0300

    Mod.lua: Add path to menu.json

--- mod.lua
+++ mod.lua
@@ -45,7 +45,7 @@ _G.RefundMod = _G.RefundMod or {}
 RefundMod.ModPath = ModPath -- cache it
 RefundMod.SavePath = ModPath .. "refund_on_crash_save.json"
 RefundMod.CrashlogPath = Application:nice_path(os.getenv("LOCALAPPDATA") .. '/PAYDAY 2/', true) .. 'crashlog.txt'
-
+RefundMod.MenuPath = ModPath .. "menu.json"
 
 function RefundMod:Save()
     local Save = io.open(self.SavePath, "w+")
